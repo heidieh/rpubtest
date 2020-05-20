da44c5760dd1bec42ce662461cdcf4cbac14249b
Heidi El-Hosaini <heidi.elhosaini@gmail.com>
2020-05-20 23:03:20 +0200

    Updates

---
+++ precache-manifest.a65c5efb853de178c37488440f237190.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de7088a7552e7606b4b839cd07fe66ab",
+    "url": "/rpubtest/index.html"
+  },
+  {
+    "revision": "29d53c0d2f0d7fd2ac23",
+    "url": "/rpubtest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6bdb706f8d2f8cfb785",
+    "url": "/rpubtest/static/js/2.d403ed19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rpubtest/static/js/2.d403ed19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d53c0d2f0d7fd2ac23",
+    "url": "/rpubtest/static/js/main.a291d074.chunk.js"
+  },
+  {
+    "revision": "8ddd130c5ff2ff400352",
+    "url": "/rpubtest/static/js/runtime-main.f4032947.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rpubtest/static/media/logo.5d5d9eef.svg"
+  }
+]);
